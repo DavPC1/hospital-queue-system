2fdfd4e1f15a91bcb9569c56c129159af565a9cf
Luis David Pérez Catalán <112285103+DavPC1@users.noreply.github.com>
2025-11-14 02:24:58 -0600

    Temp: Desprotege crear usuario para fix

--- backend/src/routes/users.routes.js
+++ backend/src/routes/users.routes.js
@@ -1,10 +1,12 @@
+// src/routes/users.routes.js
 import { Router } from 'express';
 import { createUser, listUsers } from '../controllers/users.controller.js';
 import { requireAuth } from '../middlewares/auth.js';
-import { requireRole } from '../middlewares/roles.js'; // crea el middleware que te doy
+import { requireRole } from '../middlewares/roles.js';
 const r = Router();
 
-r.post('/', requireAuth, requireRole(['admin']), createUser);
+// r.post('/', requireAuth, requireRole(['admin']), createUser); // <-- Comentada
+r.post('/', createUser); // <-- NUEVA LÍNEA (temporalmente sin seguridad)
 r.get('/', requireAuth, requireRole(['admin']), listUsers);
 
 export default r;
